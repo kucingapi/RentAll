44fa23cba7d2666a5af88930343e401fb6abcb67
noveel bafagih <novel.bafagih@udana.id>
2022-09-12 08:02:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rentall/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.rentall
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rentall.ui.theme.RentAllTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RentAllTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RentAllTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/rentall/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.rentall.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Picton50 = Color(0xFFDFF3FD)
+val Picton100 = Color(0xFFC4E9FB)
+val Picton200 = Color(0xFFA0DBF8)
+val Picton300 = Color(0xFF6EC8F5)
+val Picton400 = Color(0xFF4ABAF1)
+val Picton500 = Color(0xFF1AA7EC)
+val Picton600 = Color(0xFF2689B9)
+val Picton700 = Color(0xFF03415F)
