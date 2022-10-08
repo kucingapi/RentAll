2b6441eef5fafa4a5e3391bcf821724ee44a184c
Fayed06 <mralfayed@gmail.com>
2022-10-09 01:35:33 +0700

    change project structure

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,14 @@
-const functions = require("firebase-functions");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const admin = require("firebase-admin");
 
 const main = express();
-const app = express();
 admin.initializeApp();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors({ origin: true }))
+main.use(bodyParser.json())
+main.use(bodyParser.urlencoded({ extended: false }))
+main.use(cors({ origin: true }))
 
-exports.users = require("./user");
+
+exports.users = require("./controllers/user.controllers");
